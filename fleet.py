f686f1a57f9ac8bac321089855c1db71ebd37a61
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2021-09-09 23:10:23 -0500

    added weapons to robots

--- fleet.py
+++ fleet.py
@@ -6,8 +6,15 @@ class Fleet:
         self.fleet= []
     
     def create_fleet(self):
-        robo1= Fleet("matilla", 20, 100)
-        robo2= Fleet("metal man", 30, 100)
-        robo3= Fleet("bolts", 25, 100)
+        robo1= Robot("matilla", 15, 100)
+        robo2= Robot("metal man", 20, 100)
+        robo3= Robot("bolts", 10, 100)
         return[robo1,robo2,robo3]
+
+    def creat_weapon(self):
+        weapon1= Weapon("saw hand", 10)
+        weapon2= Weapon("lasers", 5)
+        weapon3= Weapon("chopping", 15)
+
+    
 
--- weapon.py
+++ weapon.py
@@ -1,5 +1,7 @@
 class Weapon:
-    def __init__(self, name, attack_power) -> None:
+    def __init__(self, name, attack_power):
         self.name= name
         self.attack_power= attack_power
+
+    
 
