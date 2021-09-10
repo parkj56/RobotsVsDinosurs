47903b48c68f1c18ef9e32238ee7ad20852a0b77
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2021-09-10 08:18:31 -0500

    added welcome to the game, and run game feature.

--- battlefield.py
+++ battlefield.py
@@ -4,7 +4,43 @@ from herd import Herd
 
 class Battlefield:
     def __init__(self):
-        fleet= Fleet
-        herd= Herd
-        print(Battlefield)
+        self.robofleet= Fleet().robots
+        self.dinofleet= Herd().dinosaur
 
+    def display_welcome(self):
+        print("Welcome to the Dinosaurs Vs Robots game!")
+        play_game= input("press 1 to start the battle!")
+        
+        if play_game == 1:
+            return robo_turn
+    
+    def robo_turn(self, robot):
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def run_game(self):
+        self.display_welcome()
+        while len(self.dinosaurs_defeated) or len(self.robot_defeated) < 3:
+            self.battle()
+            self.robots_turn()
+            self.dinosaurs_turn()
+        self.display_winner()
